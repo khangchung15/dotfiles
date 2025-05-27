7a4e0bb3bf1b44f721d5942904ba16fb1b9d76fa
Haha <kmchung@cougarnet.uh.edu>
2025-05-27 14:36:47 -0500

    Delete hypr directory

--- hypr/scripts/launch-waybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Kill any existing instances
-pkill waybar
-# Wait a moment
-sleep 1
-# Launch with explicit config
-/usr/bin/waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css
